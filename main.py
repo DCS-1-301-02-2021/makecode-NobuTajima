1f20fa68ab645042a0b459d6e08f0712db5c4ffc
NobuTajima <86189871+NobuTajima@users.noreply.github.com>
2021-07-26 13:09:16 +0900

    ゲーム作成中

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
 scene.set_background_color(8)
+spacePlane=sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . 9 . . . . . . . .
+    . . . . . . . 9 9 . . . . . . .
+    . . . . . . . 9 9 9 . . . . . .
+    . . . 9 . . . 9 2 9 9 . . . . .
+    . 2 2 9 9 . . 9 9 9 9 9 . . . .
+    2 2 5 9 9 9 9 9 9 9 9 1 9 9 . .
+    2 2 5 9 9 9 9 9 9 9 9 1 9 9 . .
+    . 2 2 9 9 . . 9 9 9 9 9 . . . .
+    . . . 9 . . . 9 2 9 9 . . . . .
+    . . . . . . . 9 9 9 . . . . . .
+    . . . . . . . 9 9 . . . . . . .
+    . . . . . . . 9 . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""),SpriteKind.player)
+info.set_life(3)
+spacePlane.set_stay_in_screen(True)
+controller.move_sprite(spacePlane,200,200)
+def on_a_pressed():
+    missile=sprites.create_projectile_from_sprite(
+        img("""
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            2 2 3 5 3 5 3 5 5 5 5 5 5 5 7 .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        """),spacePlane, 200, 0)
+controller.A.on_event(
+    ControllerButtonEvent.PRESSED, on_a_pressed)
+def on_update():
+    bogy=sprites.create(assets.image("""bogy"""),
+        SpriteKind.enemy)
+    bogy.set_velocity(-100,randint(-30,30))
+    bogy.y=randint(0,scene.screen_height())
+    bogy.left=scene.screen_width()
+    bogy.set_flag(SpriteFlag.AUTO_DESTROY,True)
+game.on_update_interval(500, on_update)
+def on_hit(sprite, othersprite):
+    othersprite.destroy(effects.fire,100)
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.projectile,SpriteKind.enemy,
+    on_hit)
+def on_crash(sprite, othersprite):
+    othersprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player,SpriteKind.enemy,
+    on_hit)
--- main.ts
+++ main.ts
@@ -1,1 +1,53 @@
 scene.setBackgroundColor(8)
+let spacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . 9 . . . . . . . .
+    . . . . . . . 9 9 . . . . . . .
+    . . . . . . . 9 9 9 . . . . . .
+    . . . 9 . . . 9 2 9 9 . . . . .
+    . 2 2 9 9 . . 9 9 9 9 9 . . . .
+    2 2 5 9 9 9 9 9 9 9 9 1 9 9 . .
+    2 2 5 9 9 9 9 9 9 9 9 1 9 9 . .
+    . 2 2 9 9 . . 9 9 9 9 9 . . . .
+    . . . 9 . . . 9 2 9 9 . . . . .
+    . . . . . . . 9 9 9 . . . . . .
+    . . . . . . . 9 9 . . . . . . .
+    . . . . . . . 9 . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Player)
+info.setLife(3)
+spacePlane.setStayInScreen(true)
+controller.moveSprite(spacePlane, 200, 200)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    let missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            2 2 3 5 3 5 3 5 5 5 5 5 5 5 7 .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, spacePlane, 200, 0)
+})
+game.onUpdateInterval(500, function on_update() {
+    let bogy = sprites.create(assets.image`bogy`, SpriteKind.Enemy)
+    bogy.setVelocity(-100, randint(-30, 30))
+    bogy.y = randint(0, scene.screenHeight())
+    bogy.left = scene.screenWidth()
+    bogy.setFlag(SpriteFlag.AutoDestroy, true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_hit(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
